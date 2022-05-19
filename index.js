cc91dc505268bf641664a4405d587f854bc4853a
Vishnu Kumar <vklucifer80@gmail.com>
2022-05-19 18:57:49 +0530

    Make a simple API

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.warn("Code step by step");
-console.log(100 + 29);
+const http = require("http");
+const data = require("./data");
+http
+  .createServer((req, resp) => {
+    resp.writeHead(200, { "Content-Type": "applicationjson" });
+    resp.write(JSON.stringify(data));
+    resp.end();
+  })
+  .listen(5000);
