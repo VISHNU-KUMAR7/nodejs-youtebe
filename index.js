774afbf0b0be5eb606fa336055ac52953b002662
Vishnu Kumar <vklucifer80@gmail.com>
2022-05-19 23:39:29 +0530

    Express Js

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const express = require("express");
 
 const app = express();
 app.get("", (request, response) => {
-  response.send("hello this is Home page");
+  response.send("<h1>hello this is Home page</h1>");
 });
 
 app.get("/about", (request, response) => {
-  response.send("Hello this is About Page");
+  //   response.send("Hello this is About Page");
+  response.send(
+    `<input type='text' placeholder="user Name" />
+      <button> Click me</button>`
+  );
 });
 
-app.get('/help',(request, response)=>{
-    response.send("this is the help ");
-})
+app.get("/help", (request, response) => {
+  response.send({
+    name: "vishnu",
+    emailId: "test@test.com",
+  });
+});
 
 app.listen(5000);
