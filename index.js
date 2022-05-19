6dd5d93b5150ed3911d6a817d2d21333ca16883f
Vishnu Kumar <vklucifer80@gmail.com>
2022-05-20 00:07:16 +0530

    Remove extension from URL

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+const { response } = require("express");
 const express = require("express");
-const path = require('path')
+const path = require("path");
 
 const app = express();
-const publicPath = path.join(__dirname, 'public');
-// console.log(publicPath)
+const publicPath = path.join(__dirname, "public");
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+
+app.get("", (_, response) => {
+  response.sendFile(`${publicPath}/index.html`);
+});
+
+app.get("/about", (_, response) => {
+  response.sendFile(`${publicPath}/home.html`);
+});
+
+app.get("/helpme", (_, response) => {
+  response.sendFile(`${publicPath}/help.html`);
+});
+
+app.get("*", (_, response) => {
+  response.sendFile(`${publicPath}/pagenotfound.html`);
+});
 
 app.listen(5000);
