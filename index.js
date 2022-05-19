7c782b8c6ae71e9fb9c8407daeef5abf46a62b72
Vishnu Kumar <vklucifer80@gmail.com>
2022-05-19 20:31:20 +0530

    Hangle Asynchronous Data in NodeJs

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// console.log("start exe...");
+const { rejects } = require("assert");
+const { resolve } = require("path");
 
 let a = 10;
 let b = 0;
 
-// console.log("logic exe....");
-setTimeout(() => {
-  //   console.log("logic exe...");
-  b = 20;
-}, 2000);
+let waitingData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(20);
+  }, 2000);
+});
 
-// console.log("complete exe....");
-console.log(a + b);
+waitingData.then((data) => {
+  console.log(a + data);
+});
+
+// console.log(a + b); // 10
+
+// how to get output 30 i.e 10 + 20
