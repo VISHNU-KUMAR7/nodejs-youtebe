204d0723f38fbe5387409fdfa338c193b2e55687
Vishnu Kumar <vklucifer80@gmail.com>
2022-05-19 19:10:39 +0530

    Input from Command Line

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const http = require("http");
-const data = require("./data");
-http
-  .createServer((req, resp) => {
-    resp.writeHead(200, { "Content-Type": "applicationjson" });
-    resp.write(JSON.stringify(data));
-    resp.end();
-  })
-  .listen(5000);
+const fs = require('fs');
+
+const input = process.argv;
+// fs.writeFileSync(input[2],input[3])
+
+if(input[2] == 'add'){
+    fs.writeFileSync(input[3],input[4])
+}
+else if(input[2] == 'remove'){
+    fs.unlinkSync(input[3]);
+}
+else{
+    console.log("invalid input");
+}
+
+// Command
+// node index.js hello hi
+
+// node index.js add orange.txt `this is color and fruit`
+
+// node index.js remove orange.txt
+
+// node index.js remove orange.txt
+
+// node index.js remove apple.txt 
