53ec86ba92ca6d17f0779e31cb72671fa7b5204f
Vishnu Kumar <vklucifer80@gmail.com>
2022-05-22 07:44:12 +0530

    GET, DELETE and PUT API

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const { request } = require("express");
 const express = require("express");
 require("./config");
 const Product = require("./product");
@@ -8,8 +9,25 @@ const app = express();
 app.post("/create", async (request, response) => {
   let data = new Product(request.body);
   let result = await data.save();
-  console.log(request.body)
+  console.log(request.body);
   response.send("Done");
+});
+
+app.get("/list", async (request, response) => {
+  let data = await new Product.find();
+  response.send(data);
+});
+
+app.delete("/delete/:_id", async (request, response) => {
+  console.log(request.params.id);
+  let data = await Product.deleteOne(request.params);
+  request.send("done");
+});
+
+app.put("/update/:_id", async (request, response) => {
+  const data = await new Product.updateOne(request.params, {
+    $set: request.body,
+  });
 });
 
 app.listen(5000);
