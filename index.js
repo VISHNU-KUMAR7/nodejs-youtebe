6cc32484b69baa419aac3246efdc72851682598d
Vishnu Kumar <vklucifer80@gmail.com>
2022-05-22 07:56:35 +0530

    Search API in Node js with MOngoDB

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
-const { response } = require("express");
-const { request } = require("express");
 const express = require("express");
 require("./config");
 const Product = require("./product");
-
 const app = express();
 
-app.post("/create", async (request, response) => {
-  let data = new Product(request.body);
-  let result = await data.save();
-  console.log(request.body);
-  response.send("Done");
-});
+app.use(express.json);
 
-app.get("/list", async (request, response) => {
-  let data = await new Product.find();
-  response.send(data);
-});
-
-app.delete("/delete/:_id", async (request, response) => {
-  console.log(request.params.id);
-  let data = await Product.deleteOne(request.params);
-  request.send("done");
-});
-
-app.put("/update/:_id", async (request, response) => {
-  const data = await new Product.updateOne(request.params, {
-    $set: request.body,
-  });
+app.get("/search/:key", async (request, response) => {
+  const data = await new Product.find(
+    {
+      "$or":[
+        {name:{$regex: request.params.key}},
+        {brand:{$regex: request.params.key}}
+      ]
+    }
+    )
+  response.send("done");
 });
 
 app.listen(5000);
