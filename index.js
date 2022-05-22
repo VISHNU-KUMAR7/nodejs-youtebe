f1c4ab21d727efde9f7fa4e984d3810cbea03226
Vishnu Kumar <vklucifer80@gmail.com>
2022-05-22 09:55:01 +0530

    Node js POST API with MySQL

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { request } = require("express");
+const { response } = require("express");
 const express = require("express");
 const app = express();
 
@@ -7,6 +9,21 @@ app.get("/", (request, response) => {
   connection.query("Select * from users", (error, result) => {
     error ? console.log("error") : response.send(result);
   });
+});
+
+
+//middleware
+app.use(express.json());
+
+app.post("/", (request, response) => {
+  const data = { name: "root", password: "root" };
+  connection.query(
+    "insert INTO users SET?",
+    request.body,
+    (error, result, fields) => {
+      error ? console.log("error") : response.send(result);
+    }
+  );
 });
 
 app.listen(5000);
